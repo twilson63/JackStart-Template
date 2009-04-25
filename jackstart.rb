aed7bf82f007c5334c7f4834202503a475068b39
Tom Wilson <tom@jackhq.com>
2009-04-25 08:09:15 -0400

    update layouts

--- jackstart.rb
+++ jackstart.rb
@@ -11,10 +11,11 @@ inside() do
   run "sudo gem install twilson63-nifty-generators --no-rdoc --no-ri"
   run "haml --rails ."
   run "rm public/index.html"
-  run "rm public/javascripts/control.js"
-  run "rm public/javascripts/effect.js"
+  run "rm public/javascripts/controls.js"
+  run "rm public/javascripts/effects.js"
   run "rm public/javascripts/dragdrop.js"
   run "rm public/javascripts/prototype.js"
+  run "rm public/javascripts/application.js"
   
 end
 

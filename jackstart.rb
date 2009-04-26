3de782cdfc05a6b1e5525dcedf2bb3b1cee07cdb
Tom Wilson <tom@jackhq.com>
2009-04-26 07:32:16 -0400

    update blog template

--- jackstart.rb
+++ jackstart.rb
@@ -173,7 +173,33 @@ end
 World(NavigationHelpers)
 
   CODE
-    
+  
+  file 'app/models/post.rb', <<-CODE
+class Post < ActiveRecord::Base
+  has_many :comments
+end  
+  
+  CODE
+  
+  file 'config/routes.rb', <<-CODE
+
+ActionController::Routing::Routes.draw do |map|
+  map.signup 'signup', :controller => 'users', :action => 'new'
+  map.logout 'logout', :controller => 'sessions', :action => 'destroy'
+  map.login 'login', :controller => 'sessions', :action => 'new'
+  map.resources :sessions
+
+  map.resources :users
+
+  map.resources :posts, :has_many => [:comments]
+  
+  map.root :controller => "posts"
+  
+  map.connect ':controller/:action/:id'
+  map.connect ':controller/:action/:id.:format'
+end
+  
+  CODE  
 
   
 end
